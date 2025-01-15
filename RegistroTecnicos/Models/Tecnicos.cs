4a22b1396cf2d5940d3e3af23849788eb293189d
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-01-15 16:25:25 -0500

    Hice el modelo Tecnicos

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Nombres { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[0-9\s]+$", ErrorMessage = "Solo se permiten numeros")]
+    public double SueldoHora { get; set; }
+}
