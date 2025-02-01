0941da8e05c9c6e3e7b9317b582532c4e3641e06
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-01 15:41:30 -0500

    Modelo Tickets

---
+++ RegistroTecnicos/Models/Tickets.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public DateOnly Fecha { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string Prioridad { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Asunto { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public TimeSpan TiempoInvertido { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public int TecnicoId { get; set; }
+
+    [ForeignKey("ClienteId")]
+    public Clientes Clientes { get; set; }
+
+    [ForeignKey("TecnicoId")]
+    public Tecnicos Tecnicos { get; set; }
+
+}
