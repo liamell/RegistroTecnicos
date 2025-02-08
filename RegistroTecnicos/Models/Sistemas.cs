9867a49d3bc57e6786b4c8ac398e9ce841f8c87e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-02-07 23:23:36 -0500

    Modelo

---
+++ RegistroTecnicos/Models/Sistemas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Sistemas
+{
+    [Key]
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    public string Descripcion{ get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string Complejidad { get; set; }
+}
